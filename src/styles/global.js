000f1a51803ef324e59d8b54305eae6fbd654b93
Vitor Galdino <httpvittor@gmail.com>
2022-11-29 19:26:43 -0300

    feat: created GlobalStyle component and call on main

---
+++ src/styles/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --color-primary: #FF577F;
+        --color-primary-focus: #FF427F;
+        --color-primary-negative: #59323F;
+
+        --color-grey-0: #F8F9FA;
+        --color-grey-1: #868E96;
+        --color-grey-2: #343B41;
+        --color-grey-3: #212529;
+        --color-grey-4: #121214;
+
+        --color-success: #3FE864;
+        --color-negative: #E83F5B;
+    }
+`;
