56bfeae6f8ab369638cb7122c73009aac8637b5d
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 19:17:34 -0300

    style: add height 100vh

--- src/styles/container.js
+++ src/styles/container.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     max-width: 1200px;
+    height: 100vh;
     margin: auto;
     padding: 10px;
+    background: red;
 `;
