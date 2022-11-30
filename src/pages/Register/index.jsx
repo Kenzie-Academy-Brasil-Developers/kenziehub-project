d6b205779101c8f271e376fc5d106f56a40941f6
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 20:27:06 -0300

    feat: created Register page and component

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+import { BrandTitle } from '../../components/BrandTitle';
+import { FormRegister } from '../../components/Form/FormRegister';
+
+import { MediumButton } from '../../styles/Buttons';
+import { Container } from '../../styles/Container';
+import { RegisterCard, RegisterContainer } from './styles';
 
 export const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <Container>
+      <RegisterContainer>
+        <div className='header__register'>
+          <BrandTitle fontSize='24px' />
+          <MediumButton type='button'>Voltar</MediumButton>
+        </div>
+        <RegisterCard>
+          <h3>Crie sua conta</h3>
+          <span>Rapido e grátis, vamos nessa</span>
+          <FormRegister />
+        </RegisterCard>
+      </RegisterContainer>
+    </Container>
+  );
+};
