0a06e3da6d43e362fedefce972ab3a336abd5a11
Vitor Galdino <httpvittor@gmail.com>
2022-11-29 21:20:19 -0300

    feat: created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import { MainRoutes as Routes } from './routes';
+
 export const App = () => {
   return (
-    <div></div>
+    <>
+      <Routes />
+    </>
   );
 };
