6f19fc22b2fcb1e1b3baee5d49b1c798943b0117
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 15:08:49 -0300

    feat: created DefaultInput styled component

---
+++ src/styles/Inputs.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const DefaultInput = styled.input`
+    font-family: var(--font-family);
+    color: var(--color-grey-1);
+    font-weight: 400;
+    font-size: 1.0152rem;
+    line-height: 1.625rem;
+    background: var(--color-grey-2);
+    border: 1.2182px solid var(--color-grey-2);
+    padding: 0 1.0152rem;
+    height: 3rem;
+    border-radius: 4px;
+    outline: none;
+
+    &:focus {
+        border: 1.2182px solid var(--color-grey-0);
+        color: var(--color-grey-0);
+    }
+
+    &:focus::placeholder {
+        color: var(--color-grey-0);
+    }
+`;
