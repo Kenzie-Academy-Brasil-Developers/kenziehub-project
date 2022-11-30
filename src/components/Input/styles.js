59c89e26ce1e2855565c710a086e93bd06bafb8e
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 15:18:18 -0300

    feat: created input styled component

---
+++ src/components/Input/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin-bottom: 1.3125rem;
+
+    label {
+        font-weight: 400;
+        font-size: .75rem; 
+        color: var(--color-grey-0);
+    }
+`;
