b2dcdef20f17b80579c24ec192ba866cf036dbc7
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 17:11:07 -0300

    feat: add DefaultLabel styled component to Input

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import { DefaultInput } from '../../styles/Inputs';
+import { DefaultLabel } from '../../styles/Labels';
 import { InputContainer } from './styles';
 
 export const Input = ({ type, placeholder, label, register }) => {
     return (
         <InputContainer>
-            <label htmlFor={label.toLowerCase()}>{label}</label>
+            <DefaultLabel htmlFor={label.toLowerCase()}>{label}</DefaultLabel>
             <DefaultInput type={type} id={label.toLowerCase()} placeholder={placeholder} {...register} />
         </InputContainer>
     );
