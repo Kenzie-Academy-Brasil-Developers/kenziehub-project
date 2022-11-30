b67abbf5d057d353c7f0ebfc320584f600713917
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 17:55:02 -0300

    feat: add onChange functon with callback

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -4,11 +4,17 @@ import { DefaultInput } from '../../styles/Inputs';
 import { DefaultLabel } from '../../styles/Labels';
 import { InputContainer } from './styles';
 
-export const Input = ({ type, placeholder, label, register }) => {
+export const Input = ({ type, placeholder, label, register, onChange }) => {
     return (
         <InputContainer>
             <DefaultLabel htmlFor={label.toLowerCase()}>{label}</DefaultLabel>
-            <DefaultInput type={type} id={label.toLowerCase()} placeholder={placeholder} {...register} />
+            <DefaultInput
+                type={type}
+                id={label.toLowerCase()}
+                placeholder={placeholder}
+                {...register}
+                onChange={(e) => onChange(e.target.value)}
+            />
         </InputContainer>
     );
 };
