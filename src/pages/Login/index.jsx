acae1afde614ab445725a918adb7b951aaab1ba5
Vitor Galdino <httpvittor@gmail.com>
2022-11-30 19:38:46 -0300

    feat: created Login page and component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import { BrandTitle } from '../../components/BrandTitle';
+import { FormLogin } from '../../components/Form/FormLogin';
+
+import { Button } from '../../styles/Buttons';
+import { Container } from '../../styles/Container';
+import { CardLogin, LoginContainer } from './styles';
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container>
+      <LoginContainer>
+        <BrandTitle fontSize='24px' />
+        <CardLogin>
+          <h3>Login</h3>
+          <FormLogin />
+          <span>Ainda não possui uma conta?</span>
+          <Button type='button' variant='disabled'>Cadastre-se</Button>
+        </CardLogin>
+      </LoginContainer>
+    </Container>
+  );
+};
