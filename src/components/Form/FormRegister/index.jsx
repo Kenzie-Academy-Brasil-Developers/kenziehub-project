8a519e7469cfb380a28f5171c6d45507b328a6fd
Vitor Galdino <httpvittor@gmail.com>
2022-12-01 16:20:31 -0300

    refactor: created children prop with select opttion

--- src/components/Form/FormRegister/index.jsx
+++ src/components/Form/FormRegister/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Button } from '../../../styles/Buttons';
 
+import { Button } from '../../../styles/Buttons';
 import { Input } from '../../Input';
 import { Select } from '../../Select/SelectModule';
 
@@ -13,7 +13,13 @@ export const FormRegister = () => {
             <Input type='password' placeholder='Digite novamente sua senha' label='Confirmar Senha' register='' />
             <Input type='text' placeholder='Fale sobre você' label='Bio' register='' />
             <Input type='text' placeholder='Opção de contato' label='Contato' register='' />
-            <Select id='select-module' register='' />
+            <Select id='select-module' register=''>
+                <option defaultValue={true} value="">Selecione seu módulo</option>
+                <option value="module">Primeiro módulo (Introdução ao Frontend)</option>
+                <option value="module">Segundo módulo (Frontend Avançado)</option>
+                <option value="module">Terceiro módulo (Introdução ao Backend)</option>
+                <option value="module">Quarto módulo (Backend Avançado)</option>
+            </Select>
             <Button type='submit' variant='primary'>Cadastrar</Button>
         </form>
     );
--- src/components/Select/SelectModule/index.jsx
+++ src/components/Select/SelectModule/index.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { DefaultLabel } from '../../../styles/Labels';
 
+import { DefaultLabel } from '../../../styles/Labels';
 import { DefaultSelect } from '../../../styles/Selects';
 import { SelectContainer } from './styles';
 
-export const Select = ({ id, register }) => {
+export const Select = ({ id, register, children }) => {
   return (
     <SelectContainer>
       <DefaultLabel htmlFor={id}>Selecionar Módulo</DefaultLabel>
       <DefaultSelect id={id} {...register}>
-        <option defaultValue={true} value="">Selecione seu módulo</option>
-        <option value="module">Primeiro módulo (Introdução ao Frontend)</option>
-        <option value="module">Segundo módulo (Frontend Avançado)</option>
-        <option value="module">Terceiro módulo (Introdução ao Backend)</option>
-        <option value="module">Quarto módulo (Backend Avançado)</option>
+        {children}
       </DefaultSelect>
     </SelectContainer>
   );
