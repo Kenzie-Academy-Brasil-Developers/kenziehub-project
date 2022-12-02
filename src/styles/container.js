d5f910ded6de04e41282954ad53c6756e574a703
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 08:04:38 -0300

    style: adjust max-width to 900px

--- src/styles/container.js
+++ src/styles/container.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
-    max-width: 1200px;
+    max-width: 900px;
     height: 100vh;
     margin: auto;
     padding: 10px;
