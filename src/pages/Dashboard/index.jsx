9d19cbb551d1184662c2974b9ee646c1640fe21f
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 08:32:52 -0300

    feat: included Header component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { Header } from '../../components/Header';
 import { Navbar } from '../../components/Navbar';
 
 export const Dashboard = ({ user, setUser }) => {
   return (
     <>
       <Navbar setUser={setUser} />
+      <Header user={user} />
     </>
   );
 };
