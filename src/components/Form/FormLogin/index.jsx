a13b0cd2aec7f8bc5ecc4e5c0778750edb3b169f
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 00:22:12 -0300

    feat: created fetch api to make user login

--- src/components/Form/FormLogin/index.jsx
+++ src/components/Form/FormLogin/index.jsx
@@ -1,23 +1,30 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { instance } from '../../../services/api';
 
 import { Button } from '../../../styles/Buttons';
 import { Input } from '../../Input';
 import { loginSchema } from './loginSchema';
 
 export const FormLogin = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
         mode: 'onChange',
         resolver: yupResolver(loginSchema)
     });
 
-    const handleForm = (data) => {
-        console.log(data);
+    const fetchApi = async (data) => {
+        try {
+            const response = await instance.post('/sessions', data);
+            console.log(response.data)
+            reset();
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
-        <form noValidate onSubmit={handleSubmit(handleForm)}>
+        <form noValidate onSubmit={handleSubmit(fetchApi)}>
             <Input type='email' placeholder='Digite aqui seu email' label='Email' register={register('email')} errors={errors.email} />
             <Input type='password' placeholder='Digite aqui sua senha' label='Senha' register={register('password')} errors={errors.password} />
             <Button type='submit' variant='primary'>Entrar</Button>
