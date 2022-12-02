ed987d4ba35ef2b759d82adfb51b4f3602005372
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 08:23:44 -0300

    style: change viewport height to porcentage on height property

--- src/styles/container.js
+++ src/styles/container.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     max-width: 900px;
-    height: 100vh;
+    height: 100%;
     margin: auto;
     padding: 10px;
 `;
