2104c0cf27179036078f817f6cd8d29b35f4233e
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 08:47:13 -0300

    feat: add Main component

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Header } from '../../components/Header';
+import { Main } from '../../components/Main';
 import { Navbar } from '../../components/Navbar';
 
 export const Dashboard = ({ user, setUser }) => {
@@ -8,6 +9,7 @@ export const Dashboard = ({ user, setUser }) => {
     <>
       <Navbar setUser={setUser} />
       <Header user={user} />
+      <Main />
     </>
   );
 };
