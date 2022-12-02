d06d1c6f9135ed6c635ea8713379f4c33a304590
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 00:26:07 -0300

    feat: created loading state

--- src/components/Form/FormLogin/index.jsx
+++ src/components/Form/FormLogin/index.jsx
@@ -1,5 +1,5 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { instance } from '../../../services/api';
 
@@ -8,6 +8,7 @@ import { Input } from '../../Input';
 import { loginSchema } from './loginSchema';
 
 export const FormLogin = () => {
+    const [loading, setLoading] = useState(false);
     const { register, handleSubmit, formState: { errors }, reset } = useForm({
         mode: 'onChange',
         resolver: yupResolver(loginSchema)
@@ -15,11 +16,14 @@ export const FormLogin = () => {
 
     const fetchApi = async (data) => {
         try {
+            setLoading(true);
             const response = await instance.post('/sessions', data);
-            console.log(response.data)
+            console.log(response.data);
             reset();
         } catch (err) {
             console.error(err);
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -27,7 +31,7 @@ export const FormLogin = () => {
         <form noValidate onSubmit={handleSubmit(fetchApi)}>
             <Input type='email' placeholder='Digite aqui seu email' label='Email' register={register('email')} errors={errors.email} />
             <Input type='password' placeholder='Digite aqui sua senha' label='Senha' register={register('password')} errors={errors.password} />
-            <Button type='submit' variant='primary'>Entrar</Button>
+            <Button type='submit' disabled={loading} variant={loading ? 'negative' : 'primary'}>Cadastrar</Button>
         </form>
     );
 };
