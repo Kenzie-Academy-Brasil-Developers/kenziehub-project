a26dec7a20aed79d720ae8dec275012b8cb17f20
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 09:51:58 -0300

    fix: change Global name to default with capitalize

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
-import { GlobalStyle } from './styles/global';
+import { GlobalStyle } from './styles/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
