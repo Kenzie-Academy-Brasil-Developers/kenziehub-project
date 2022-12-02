26dda67bbf73e99f0d71523516f0ee2343c76df5
Vitor Galdino <httpvittor@gmail.com>
2022-12-01 23:29:21 -0300

    feat: included useForm and yupResolver

--- src/components/Form/FormRegister/index.jsx
+++ src/components/Form/FormRegister/index.jsx
@@ -1,24 +1,38 @@
+import { yupResolver } from '@hookform/resolvers/yup';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 import { Button } from '../../../styles/Buttons';
 import { Input } from '../../Input';
 import { Select } from '../../Select/SelectModule';
+import { registerSchema } from './registerSchema';
 
 export const FormRegister = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+        mode: 'onChange',
+        resolver: yupResolver(registerSchema)
+    });
+
+    const handleForm = (data) => {
+        delete data.confirmPwd;
+        console.log(data);
+        reset();
+    };
+
     return (
-        <form>
-            <Input type='text' placeholder='Digite aqui seu nome' label='Nome' register='' />
-            <Input type='email' placeholder='Digite aqui seu email' label='Email' register='' />
-            <Input type='password' placeholder='Digite aqui sua senha' label='Senha' register='' />
-            <Input type='password' placeholder='Digite novamente sua senha' label='Confirmar Senha' register='' />
-            <Input type='text' placeholder='Fale sobre você' label='Bio' register='' />
-            <Input type='text' placeholder='Opção de contato' label='Contato' register='' />
-            <Select id='select-module' register=''>
+        <form noValidate onSubmit={handleSubmit(handleForm)}>
+            <Input type='text' placeholder='Digite aqui seu nome' label='Nome' register={register('name')} errors={errors.name} />
+            <Input type='email' placeholder='Digite aqui seu email' label='Email' register={register('email')} errors={errors.email} />
+            <Input type='password' placeholder='Digite aqui sua senha' label='Senha' register={register('password')} errors={errors.password} />
+            <Input type='password' placeholder='Digite novamente sua senha' label='Confirmar Senha' register={register('confirmPwd')} errors={errors.confirmPwd} />
+            <Input type='text' placeholder='Fale sobre você' label='Bio' register={register('bio')} errors={errors.bio} />
+            <Input type='text' placeholder='Opção de contato' label='Contato' register={register('contact')} errors={errors.contact} />
+            <Select id='select-module' register={register('course_module')} errors={errors.course_module} >
                 <option defaultValue={true} value="">Selecione seu módulo</option>
-                <option value="module">Primeiro módulo (Introdução ao Frontend)</option>
-                <option value="module">Segundo módulo (Frontend Avançado)</option>
-                <option value="module">Terceiro módulo (Introdução ao Backend)</option>
-                <option value="module">Quarto módulo (Backend Avançado)</option>
+                <option value="Primeiro módulo (Introdução ao Frontend)">Primeiro módulo</option>
+                <option value="Segundo módulo (Frontend Avançado)">Segundo módulo</option>
+                <option value="Terceiro módulo (Introdução ao Backend)">Terceiro módulo</option>
+                <option value="Quarto módulo (Backend Avançado)">Quarto módulo</option>
             </Select>
             <Button type='submit' variant='primary'>Cadastrar</Button>
         </form>
