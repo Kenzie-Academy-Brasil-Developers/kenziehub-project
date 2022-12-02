8c656e47e7587091d35fc93a3d203b1d849f20e2
Vitor Galdino <httpvittor@gmail.com>
2022-12-02 08:05:37 -0300

    feat: created NavbarContainer styled component

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NavbarContainer = styled.div`
+    & > div {
+        display: flex;
+        justify-content: space-between;
+        padding: 20px 0;
+    }
+`;
