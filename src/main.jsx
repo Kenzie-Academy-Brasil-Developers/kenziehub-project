1eb9a32f1bd2c8bf549668f2bf1393f896429e3e
Vitor Galdino <httpvittor@gmail.com>
2022-12-04 20:17:56 -0300

    feat: call Providers component on App

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
+import { Providers } from './contexts';
 import { GlobalStyle } from './styles/Global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+      <Providers>
+        <GlobalStyle />
+        <App />
+      </Providers>
     </BrowserRouter>
   </React.StrictMode>
 );
