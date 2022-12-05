72bf620bad4ea1290d907b5ba10278dd259f0a4c
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 15:31:33 -0300

    refactor: change state name to setRegisModal

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,19 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { Main } from '../../components/Main';
+import { RegisTechModal } from '../../components/Modal/RegistTechModal';
 import { Navbar } from '../../components/Navbar';
+import { TechContext } from '../../contexts/techContext';
 
 export const Dashboard = () => {
+  const { regisModal } = useContext(TechContext);
+
   const navigate = useNavigate();
-  
-  useEffect(() =>  {
+
+  useEffect(() => {
     if (!localStorage.userToken) navigate('/');
   }, []);
 
   return (
     <>
+      {regisModal && <RegisTechModal />}
       <Navbar />
       <Header />
       <Main />
