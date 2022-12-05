30f46b119b4a8d036d6aaaed3c721d3e1289986f
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 08:45:37 -0300

    feat: created Modal styles

---
+++ src/components/Modal/RegistTechModal/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Modal = styled.div`
+    position: fixed;
+    inset: 0;
+    z-index: 100;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--color-overlay-grey-0);
+`;
+
+export const ModalWrapper = styled.div`
+
+`;
+
+export const ModalHeader = styled.div`
+
+`;
