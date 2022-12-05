fd771538b0895cb7278b00f1a788cd66fe4acd7e
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 07:56:56 -0300

    feat: render TechCard component with techs state

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
+import { UserContext } from '../../contexts/userContext';
 import { Container } from '../../styles/Container';
 import { AddButton } from '../AddButton';
-import { TechHeader } from './styles';
+import { TechCard } from '../TechCard';
+
+import { TechHeader, TechWrapper } from './styles';
 
 export const Main = () => {
+    const { techs } = useContext(UserContext);
+
     return (
         <Container>
             <TechHeader>
                 <h3>Tecnologias</h3>
                 <AddButton />
             </TechHeader>
+            <TechWrapper>
+                {techs.map(({ title, status, id }) =>
+                    <TechCard key={id} name={title} status={status} techId={id} />)}
+            </TechWrapper>
         </Container>
     );
 };
