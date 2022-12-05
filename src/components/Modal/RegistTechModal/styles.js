ac8443c1575afc928c39cc1259b1b636d91c742b
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 09:11:59 -0300

    feat: created ModalWrapper style

--- src/components/Modal/RegistTechModal/styles.js
+++ src/components/Modal/RegistTechModal/styles.js
@@ -6,6 +6,7 @@ export const Modal = styled.div`
     z-index: 100;
     width: 100vw;
     height: 100vh;
+    padding: 10px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -13,7 +14,15 @@ export const Modal = styled.div`
 `;
 
 export const ModalWrapper = styled.div`
-
+    width: 100%;
+    max-width: 369px;
+    height: 100%;
+    max-height: 342px;
+    overflow-y: auto;
+    padding: 22px 17px;
+    background-color: var(--color-grey-3);
+    box-shadow: 0px 4px 40px -10px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
 `;
 
 export const ModalHeader = styled.div`
