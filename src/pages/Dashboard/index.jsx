06cb905a2a7db29f0971a14211aa77bfcdb40c23
Vitor Galdino <httpvittor@gmail.com>
2022-12-04 23:12:02 -0300

    feat: included user permission

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Header } from '../../components/Header';
 import { Main } from '../../components/Main';
 import { Navbar } from '../../components/Navbar';
 
 export const Dashboard = () => {
+  const navigate = useNavigate();
+  
+  useEffect(() => !localStorage.userToken && navigate('/'), []);
 
   return (
     <>
