4356182b07a3018adc2993c3dba91e1b832818e7
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 18:42:50 -0300

    feat: created createTechApi function to request api

--- src/contexts/techContext/index.jsx
+++ src/contexts/techContext/index.jsx
@@ -1,4 +1,6 @@
 import { createContext, useState } from 'react';
+import { toast } from 'react-toastify';
+import { instance } from '../../services/api';
 
 export const TechContext = createContext({});
 
@@ -6,12 +8,29 @@ export const TechProvider = ({ children }) => {
     const [techs, setTechs] = useState([]);
     const [regisModal, setRegisModal] = useState(false);
 
+    const createTechApi = async (data) => {
+        try {
+            const response = await instance.post('/users/techs', data, {
+                headers: { Authorization: `Bearer ${localStorage.userToken}` },
+            });
+
+            toast.success('Tecnologia cadastrada!');
+            setTechs((old) => [...old, response.data]);
+            setRegisModal(false);
+
+        } catch (err) {
+            toast.info('Tecnologia já cadastrada.');
+            return err;
+        }
+    };
+
     return (
         <TechContext.Provider value={{
             techs,
             setTechs,
             regisModal,
             setRegisModal,
+            createTechApi,
         }}>
             {children}
         </TechContext.Provider>
