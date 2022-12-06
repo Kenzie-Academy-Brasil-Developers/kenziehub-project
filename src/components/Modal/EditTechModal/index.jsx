95b51912b632fe32eb7ec5bf6075ec26a89cdeb2
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 21:35:25 -0300

    feat: created handleSubmitButton function

--- src/components/Modal/EditTechModal/index.jsx
+++ src/components/Modal/EditTechModal/index.jsx
@@ -1,4 +1,5 @@
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { TechContext } from '../../../contexts/techContext';
 
 import { Button, CloseButton } from '../../../styles/Buttons';
@@ -7,7 +8,12 @@ import { Select } from '../../Select/SelectModule';
 import { Modal, ModalFooter, ModalHeader, ModalWrapper } from './styles';
 
 export const EditTechModal = () => {
+    const { register, handleSubmit } = useForm({
+        mode: 'onChange',
+    });
+
     const { setEditModal, loading, modalInfo, deleteTechApi } = useContext(TechContext);
+    const [disableSubmit, setDisableSubmit] = useState(true);
     const modalRef = useRef(null);
 
     useEffect(() => {
@@ -23,16 +29,29 @@ export const EditTechModal = () => {
         return () => window.removeEventListener('mousedown', modalOutClick);
     }, []);
 
+    const handleSubmitButton = (event) => {
+        if (event.target.value === modalInfo.status) {
+            setDisableSubmit(true);
+        } else {
+            setDisableSubmit(false);
+        }
+    };
+
     return (
         <Modal ref={modalRef}>
             <ModalWrapper>
                 <ModalHeader>
                     <h4>Tecnologia Detalhes</h4>
-                    <CloseButton onClick={() => setEditModal(false)} />
+                    <CloseButton onClick={() => setEditModal()} />
                 </ModalHeader>
-                <form noValidate>
+                <form noValidate onSubmit={handleSubmit()}>
                     <Input type='text' placeholder={modalInfo.title} label='Nome' disabled={true} />
-                    <Select id='select-techs' label='Selecionar status' register='' defaultValue={modalInfo.status}>
+                    <Select
+                        id='select-techs'
+                        label='Selecionar status'
+                        register={register('status')}
+                        defaultValue={modalInfo.status}
+                        onChange={handleSubmitButton}>
                         <option value="Iniciante">Iniciante</option>
                         <option value="Intermediário">Intermediário</option>
                         <option value="Avançado">Avançado</option>
@@ -40,8 +59,8 @@ export const EditTechModal = () => {
                     <ModalFooter>
                         <Button
                             type='submit'
-                            disabled={loading}
-                            variant={loading ? 'negative' : 'primary'}
+                            disabled={loading || disableSubmit}
+                            variant={loading || disableSubmit ? 'negative' : 'primary'}
                             id={modalInfo.id}
                         >Salvar alterações</Button>
 
