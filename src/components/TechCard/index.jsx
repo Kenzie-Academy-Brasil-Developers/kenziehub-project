d37d2a44fb0aa317dc7f96929b2bc536acdc2f3d
Vitor Galdino <httpvittor@gmail.com>
2022-12-05 21:01:29 -0300

    refactor: change onClick callback on Card and remove trash button

--- src/components/TechCard/index.jsx
+++ src/components/TechCard/index.jsx
@@ -3,13 +3,12 @@ import React from 'react';
 import { TrashButton } from '../../styles/Buttons';
 import { Card } from './styles';
 
-export const TechCard = ({ name, status, techId, onClick, loading }) => {
+export const TechCard = ({ name, status, techId, onClick }) => {
     return (
-        <Card id={techId}>
+        <Card id={techId} onClick={onClick}>
             <p>{name}</p>
             <div>
                 <span>{status}</span>
-                <TrashButton id={techId} onClick={onClick} disabled={loading} />
             </div>
         </Card>
     );
