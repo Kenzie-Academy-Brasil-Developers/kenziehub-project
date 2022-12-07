0d53831820773a8386c8ed969d4aceffd923d628
Vitor Galdino <httpvittor@gmail.com>
2022-12-06 21:32:12 -0300

    feat: add loading waitScreen and included your context

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -6,10 +6,13 @@ import { Main } from '../../components/Main';
 import { EditTechModal } from '../../components/Modal/EditTechModal';
 import { RegisTechModal } from '../../components/Modal/RegistTechModal';
 import { Navbar } from '../../components/Navbar';
+import { WaitScreen } from '../../components/WaitScreen';
 import { TechContext } from '../../contexts/techContext';
+import { UserContext } from '../../contexts/userContext';
 
 export const Dashboard = () => {
   const { regisModal, editModal } = useContext(TechContext);
+  const { loadWaitScreen } = useContext(UserContext);
 
   const navigate = useNavigate();
 
@@ -19,6 +22,7 @@ export const Dashboard = () => {
 
   return (
     <>
+      {loadWaitScreen && <WaitScreen />}
       {regisModal && <RegisTechModal />}
       {editModal && <EditTechModal />}
       <Navbar />
