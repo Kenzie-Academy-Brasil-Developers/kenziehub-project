59cd725f5110c6a9d0acd493382cc3e823474743
Vitor Galdino <httpvittor@gmail.com>
2022-12-06 21:23:02 -0300

    feat: created WaitContainer styled component

---
+++ src/components/WaitScreen/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const WaitContainer = styled.div`
+    position: fixed;
+    inset: 0;
+    height: 100vh;
+    width: 100vw;
+
+    display: grid;
+    place-items: center;
+
+    background-color: var(--color-grey-4);
+`;
